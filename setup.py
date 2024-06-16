bb4be2b61db99a367f5ecd6aaa60862772835cff
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-06-16 15:19:44 +0200

    edit

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+
+def read(filename):
+    with open(filename, "r") as file:
+        return file.read()
 
 setup(
-    name='api-client-generator',
-    version='1.0.0',
-    description='A tool to automatically generate client libraries for RESTful APIs based on OpenAPI/Swagger specifications',
-    author='Your Name',
-    author_email='your.email@example.com',
-    package_dir={'':'src'},
-    packages=find_packages(where='src'),
+    name="api-client-generator",
+    version="1.0.0",
+    packages=find_packages(),
+    url="https://github.com/AbdullahBakir97/API-Client-Generator",
+    license="MIT",
+    description="A tool to automatically generate client libraries for RESTful APIs based on OpenAPI/Swagger specifications",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Radwan Hegazy",
+    author_email="radwan.hegazy@example.com",
+    python_requires=">=3.8",
+    project_urls={
+        "Documentation": "https://github.com/AbdullahBakir97/API-Client-Generator/wiki",
+        "Source": "https://github.com/AbdullahBakir97/API-Client-Generator",
+        "Bug Tracker": "https://github.com/AbdullahBakir97/API-Client-Generator/issues",
+    },
     install_requires=[
         'pyyaml',
         'jinja2',
@@ -21,28 +35,4 @@ setup(
             'generate-client=api_client_generator.generator:main',
         ],
     },
-)
-
-
-from setuptools import find_packages, setup
-
-
-def read(filename):
-    with open(filename, "r") as file:
-        return file.read()
-
-setup(
-    name="api-client-generator",
-    version="1.0.0",
-    packages=find_packages(),
-    url="https://github.com/RadwanHegazy/pythonwebui",
-    license="MIT",
-    description="Build your HTML and CSS with ease",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    author="Radwan Hegazy",
-    python_requires=">=3.8",
-    project_urls={
-        "source": "https://github.com/RadwanHegazy/pythonwebui"
-    }
 )
